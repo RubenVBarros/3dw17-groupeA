5c59990a39a3c33f794db712e950c84e72493cab
RubenVBarros <rubenvenancio67@gmail.com>
2021-02-19 17:07:57 +0100

    main: Refonte du Design

--- README.md
+++ README.md
@@ -2,30 +2,9 @@
 
 Le repo pour le projet de symfony du groupe A avec les membres Ruben BARROS et Romain VIDON
 
-## Ajouter ou Mettre à jour une entité :
+Chose à faire :
 ```
-php bin/console make:entity
-```
-
-## Préparer les données pour l'envoi dans la bdd :
-```
-php bin/console make:migration
-```
-
-## Initialiser la bdd et pousser les migrations sur la bdd :
-```
-php bin/console doctrine:migrations:migrate
-```
-
-## (RAPPEL) Types pour la création d'entité sur Doctrine :
-```
-string,
-integer,
-boolean,
-date,
-float,
-time,
-datetime,
-object,
-array,
+- Contrainte de temps pour les users normaux (effacable ou éditable si < 30 minutes).
+- Like/Dislike d'un sujet.
+- Signalement d'un sujet.
 ```
--- projet/src/Controller/CommentaireController.php
+++ projet/src/Controller/CommentaireController.php
@@ -10,47 +10,4 @@ use App\Repository\CommentaireRepository;
 
 
 class CommentaireController extends AbstractController
-{
-    /**
-     * @Route("/commentaire/create", name="commentaire_create")
-     */
-    public function createCommentaire(): Response{
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $com = new Commentaire();
-        $com->setContenu("Je pense que l'antidatage des stock-options est une chose très quantifiable");
-
-        $entityManager->persist($com);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        return new Response('Nouveau commentaire avec cet id : '.$com->getId(). '<br>'. 'Et avec ce contenu : '.$com->getContenu());
-    }
-    /**
-     * @Route("/commentaire/{id}", name="commentaire_show")
-     */
-    public function showId(int $id, CommentaireRepository $comRepository): Response{
-        $com = $comRepository
-            ->find($id);
-            return new Response('Contenu du commentaire : '.$com->getContenu());
-    }
-    /**
-     * @Route("/commentaire/edit/{id}")
-     */
-    public function update(int $id): Response {
-        $entityManager = $this->getDoctrine()->getManager();
-        $com = $entityManager->getRepository(Commentaire::class)->find($id);
-
-        if(!$com){
-            throw $this->createNotFoundException('Pas de commentaire avec cet id' . $id);
-        }
-
-        $com->setContenu("Je pense que je suis le plus fort");
-        $entityManager->flush();
-
-        return $this->redirectToRoute('commentaire_show',[
-            'id'=>$com->getId()
-        ]);
-    }
-}
+{}
